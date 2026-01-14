4c32d5f436866d90f210c2a4455b8b843fdc2654
IGORECHEKST <165463152+IGORECHEKST@users.noreply.github.com>
2026-01-14 07:58:35 +0200

    my first sol after first rew

--- target/generated-sources/annotations/com/example/demo/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/example/demo/mapper/UserMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-01-11T13:27:26+0200",
+    date = "2026-01-14T07:47:26+0200",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.1 (Oracle Corporation)"
 )
 @Component
